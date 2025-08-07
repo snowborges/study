d00e95b0a44265be9c7e3085ae1679c261fbe758
letícia <122499469+hellticia@users.noreply.github.com>
2025-08-07 17:53:44 -0300

    node modulos

--- exercicios-web/node/pastaA/pastaB/acessarModulos.js
+++ exercicios-web/node/pastaA/pastaB/acessarModulos.js
@@ -1,4 +1,4 @@
-const moduloA = require('../../moduloA')
+const moduloA = require('../../moduloA') // importa o módulo A  
 console.log(moduloA.ola)
 
 const saudacao = require('saudacao')
@@ -7,8 +7,8 @@ console.log(saudacao.ola)
 const c = require('./pastaC')
 console.log(c.ola2)
 
-const http = require('http')
+const http = require('http') // módulo http do Node.js
 http.createServer((req, res) => {
     res.write('Bom dia!')
     res.end()
-}).listen(8080)
+}).listen(8080) // cria um servidor HTTP que responde com "Bom dia!" na porta 8080
