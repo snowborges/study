da7a8fd0005d631c0a23ef89281338c565899eae
letícia <122499469+hellticia@users.noreply.github.com>
2025-08-08 16:56:26 -0300

    filesystem, leitura sincrona e assincrona

--- exercicios-web/node/arquivoLeitura.js
+++ exercicios-web/node/arquivoLeitura.js
@@ -4,18 +4,21 @@ const caminho = __dirname + '/arquivo.json'
 
 // sincrono...
 const conteudo = fs.readFileSync(caminho, 'utf-8')
-console.log(conteudo)
+console.log(conteudo) // aqui nao preciso converter em string pq o readFileSync ja retorna o conteudo do arquivo (ele pausa o codigo ate ler o arquivo)
 
 // assincrono...
 fs.readFile(caminho, 'utf-8', (err, conteudo) => {
     const config = JSON.parse(conteudo)
     console.log(`${config.db.host}:${config.db.port}`)
-})
+}) // o json.parse converte conteudo em uma string devido ao readFile não retornar imediatamente o conteudo do arquivo
 
 const config = require('./arquivo.json')
 console.log(config.db)
 
-fs.readdir(__dirname, (err, arquivos) => {
+fs.readdir(__dirname, (err, arquivos) => { // __dirname = diretorio atual do arquivo
     console.log('Conteúdo da pasta...')
     console.log(arquivos)
 })
+
+
+// fs= file system = modulo nativo do node para trabalhar com arquivos
--- exercicios-web/node/passandoParametros.js
+++ exercicios-web/node/passandoParametros.js
@@ -1,3 +1,3 @@
 module.exports = function(...nomes) {
     return nomes.map(nome => `Boa semana ${nome}!`)
-}
+} // ...nomes torna o argumento variável, permitindo passar qualquer número de nomes
