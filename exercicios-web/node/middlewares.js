4329f4e1f9d41130f68d18859018b1577099e38e
letícia <122499469+hellticia@users.noreply.github.com>
2025-08-13 20:21:34 -0300

    middleware

--- exercicios-web/node/middlewares.js
+++ exercicios-web/node/middlewares.js
@@ -1,7 +1,7 @@
 // middleware pattern (chain of responsibility)
 const passo1 = (ctx, next) => {
     ctx.valor1 = 'mid1'
-    next()
+    next()  // dispara o proximo passo de cadeia sem interromper o fluxo 
 }
 
 const passo2 = (ctx, next) => {
@@ -11,13 +11,13 @@ const passo2 = (ctx, next) => {
 
 const passo3 = ctx => ctx.valor3 = 'mid3'
 
-const exec = (ctx, ...middlewares) => {
+const exec = (ctx, ...middlewares) => { 
     const execPasso = indice => {
         middlewares && indice < middlewares.length &&
             middlewares[indice](ctx, () => execPasso(indice + 1))
     }
-    execPasso(0)
-}
+    execPasso(0) // começa a execução a partir do primeiro middleware
+} // o "..." é um operador rest que permite receber um número indefinido de middlewares
 
 const ctx = {}
 exec(ctx, passo1, passo2, passo3)
